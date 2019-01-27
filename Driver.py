7eb49fb684ac8157d31eb9ed8763f84cf6d2b9b3
Kaushik <kaushik.lvk@gmail.com>
2019-01-27 09:09:20 +0530

    Added call to read data from the tables

--- Driver.py
+++ Driver.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     path = input("Enter path to data files")
     table_data = IP.read_metadata(path)
 
-    table_list = ["sample", "table1"]
-    actual_data = IP.read_data(path, table_list)
+    table_list = ["table1", "sample"]
+    actual_data = IP.read_data(path, table_list, table_data)
+    print(actual_data)
     print(table_data)
